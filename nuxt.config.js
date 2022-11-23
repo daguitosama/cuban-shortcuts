6523f2850931f311c37881b7018e3958f70f998b
daguitosama <tocarralero@gmail.com>
2022-11-22 21:50:11 -0500

    update: nuxt-3 bump

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,63 +1,42 @@
 export default {
-  // Target: https://go.nuxtjs.dev/config-target
-  target: 'static',
+    // Target: https://go.nuxtjs.dev/config-target
+    target: "static",
 
-  // router: {
-  //   base: '/'
-  // },
+    // router: {
+    //   base: '/'
+    // },
 
-  // Global page headers: https://go.nuxtjs.dev/config-head
-  head: {
-    title: 'Atajos',
-    htmlAttrs: {
-      lang: 'es'
+    // Global page headers: https://go.nuxtjs.dev/config-head
+    head: {
+        title: "Atajos",
+        htmlAttrs: {
+            lang: "es",
+        },
+        meta: [
+            { charset: "utf-8" },
+            {
+                name: "viewport",
+                content: "width=device-width, initial-scale=1",
+            },
+            { hid: "description", name: "description", content: "" },
+            { name: "format-detection", content: "telephone=no" },
+        ],
+        link: [
+            { rel: "icon", type: "image/x-icon", href: "/favicon.ico" },
+            { rel: "manifest", href: "/manifest.json" },
+            { rel: "apple-touch-icon", href: "/touch-icon-iphone.png" },
+            // <link rel="manifest" href="manifest.json">
+            // <link rel="apple-touch-icon" href="touch-icon-iphone.png">
+        ],
+
+        bodyAttrs: {
+            class: ["min-h-screen w-full"],
+        },
     },
-    meta: [
-      { charset: 'utf-8' },
-      { name: 'viewport', content: 'width=device-width, initial-scale=1' },
-      { hid: 'description', name: 'description', content: '' },
-      { name: 'format-detection', content: 'telephone=no' },
-    ],
-    link: [
-      { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' },
-      { rel: 'manifest', href: '/manifest.json' },
-      { rel: 'apple-touch-icon', href: '/touch-icon-iphone.png' },
-      // <link rel="manifest" href="manifest.json">
-      // <link rel="apple-touch-icon" href="touch-icon-iphone.png">
 
-    ],
+    // Global CSS: https://go.nuxtjs.dev/config-css
+    css: [],
 
-    bodyAttrs: {
-      class: ['min-h-screen w-full']
-    }
-  },
-
-  // Global CSS: https://go.nuxtjs.dev/config-css
-  css: [
-  ],
-
-  // Plugins to run before rendering page: https://go.nuxtjs.dev/config-plugins
-  plugins: [
-  ],
-
-  // Auto import components: https://go.nuxtjs.dev/config-components
-  components: true,
-
-  // Modules for dev and build (recommended): https://go.nuxtjs.dev/config-modules
-  buildModules: [
-    // https://go.nuxtjs.dev/tailwindcss
-    '@nuxtjs/tailwindcss',
-    '@nuxtjs/composition-api/module',
-    // '@nuxt/image',
-    // '@nuxtjs/pwa',
-  ],
-
-  // Modules: https://go.nuxtjs.dev/config-modules
-  modules: [
-  ],
-
-  // Build Configuration: https://go.nuxtjs.dev/config-build
-  build: {
-  },
-
-}
+    // Modules: https://go.nuxtjs.dev/config-modules
+    modules: ["@nuxtjs/tailwindcss"],
+};
